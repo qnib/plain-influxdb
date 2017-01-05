3be1b0c7e653efedefd15df17199698ed2da04ba
Christian Kniep <ckniep@gaikai.com>
2017-01-05 12:52:52 +0100

    first attempt to create databases

--- opt/qnib/influxdb/bin/start.sh
+++ opt/qnib/influxdb/bin/start.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-exec influxd -pidfile /var/run/influxdb.pid -config /etc/influxdb/influxdb.conf ${INFLUXD_OPTS}
+influxd -pidfile /var/run/influxdb.pid -config /etc/influxdb/influxdb.conf ${INFLUXD_OPTS} &
+sleep 5
+if [ "X${INFLUXDB_DATABASES}" != "X" ];then
+    for db in $(echo ${INFLUXDB_DATABASES} |sed -e 's/,/ /g');do 
+        echo "[INFO] Create database ${db}"
+        influx -host localhost -port 8086 -execute "create database ${db}"
+    done
+fi
+fg
